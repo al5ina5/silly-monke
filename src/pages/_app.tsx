465cb81cee790b07e7f77b9598d3433bcba526c6
Sebastian Alsina <alsinas@me.com>
2023-02-13 19:11:29 -0500

    added meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import '../global.css'
 import axios from 'axios'
 import useSound from 'use-sound';
 import { useContext, createContext } from 'react';
+import Meta from '../components/Meta';
 
 
 export const AppContext = createContext({})
@@ -13,6 +14,7 @@ export default function MyApp({ Component, pageProps }) {
     const [play] = useSound("/audio/monke-song.mp3");
 
     return <>
+        <Meta />
         <AppContext.Provider value={{ play }}>
             <SWRConfig value={{ fetcher: url => axios.get(url).then(res => res.data) }}>
                 <Component {...pageProps} />
